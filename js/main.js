c148b35f5f07ab841c1b2385faa2f9128643e26c
julieluangpraxay <julieluangpraxay@gmail.com>
2023-08-15 16:44:27 +0000

    Testing out dummy entries

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 const $enterButton = document.querySelector('.enter-button');
-const $body = document.body;
+const $body = document.querySelector('body');
 const $firstLogo = document.querySelector('.first-logo'); // Assuming you have a .first-logo element
 const $header = document.querySelector('.header'); // Assuming you have a .header element
 const $mainSearch = document.querySelector('.search-bar-full');
-
+const $weaponList = document.querySelector('#display-name');
 // Add a click event listener to the button
 $enterButton.addEventListener('click', function () {
   // Hide the button, background image, and .first-logo
@@ -16,5 +16,8 @@ $enterButton.addEventListener('click', function () {
   $mainSearch.style.display = 'flex';
   // Show the header
   $header.style.display = 'flex';
+  // show the new background image
   $body.style.backgroundImage = 'url(../images/mainbg.jpg)';
+  // show the ul of the weapon list
+  $weaponList.style.display = 'flex';
 });
