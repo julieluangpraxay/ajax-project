b162f2ed8e692a45841f2ef0e181b50f5bd1eb4f
julieluangpraxay <julieluangpraxay@gmail.com>
2023-08-17 23:45:56 +0000

    Finished issue 1

--- js/main.js
+++ js/main.js
@@ -1,44 +1,42 @@
-
-// new code that searches and works but is slow
 const $enterButton = document.querySelector('.enter-button');
 const $background = document.querySelector('#background');
 const $loadingLogo = document.querySelector('.loading');
 const $searchBar = document.querySelector('.main-search');
-const $searchButton = document.querySelector('.search-button');
 const $skinsContainer = document.querySelector('.skins-container');
+const $header = document.querySelector('header');
+const $smallLogo = document.querySelector('.logo');
 
 $enterButton.addEventListener('click', function () {
   $enterButton.classList.add('hidden');
   $background.className = 'skin-background';
   $loadingLogo.classList.add('hidden');
   $searchBar.classList.remove('hidden');
-  $searchButton.classList.remove('hidden');
+  $header.classList.remove('hidden');
+  $smallLogo.classList.remove('hidden');
   getSkinsData();
 });
 
 function getSkinsData(name) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://valorant-api.com/v1/playercards');
+  xhr.open('GET', 'https://valorant-api.com/v1/sprays');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    $skinsContainer.innerHTML = ''; // Clear previous content
-
+    $skinsContainer.innerHTML = ''; // Clears previous content
     for (let i = 0; i < xhr.response.data.length; i++) {
-      const skinName = xhr.response.data[i].displayName.toLowerCase(); // Convert to lowercase for case-insensitive search
-
+      const skinName = xhr.response.data[i].displayName.toLowerCase();
       if (!name || skinName.includes(name.toLowerCase())) { // Check if search term is empty or matches skin name
         const $div = document.createElement('div'); // outerdiv
         const $imgWrapper = document.createElement('div');
         const $iconImage = document.createElement('img');
         const $p = document.createElement('p');
-
-        $iconImage.src = xhr.response.data[i].largeArt;
+        const $heartIcon = document.createElement('i');
+        $iconImage.src = xhr.response.data[i].fullTransparentIcon;
         $p.textContent = xhr.response.data[i].displayName;
-
-        $div.classList.add('column-third');
+        $div.classList.add('column-fourth');
         $imgWrapper.classList.add('img-wrapper');
-
+        $heartIcon.className = 'fa-regular fa-heart fa-2xl image-heart';
         $div.appendChild($imgWrapper);
+        $imgWrapper.appendChild($heartIcon);
         $imgWrapper.appendChild($iconImage);
         $div.appendChild($p);
         $skinsContainer.appendChild($div);
@@ -48,14 +46,12 @@ function getSkinsData(name) {
   xhr.send();
 }
 
+// SEARCH BAR FUNCTION
+$searchBar.addEventListener('input', searchClick);
 function searchClick(event) {
   if (event) {
     event.preventDefault();
   }
-
   const searchSkin = $searchBar.value;
   getSkinsData(searchSkin);
 }
-
-$searchButton.addEventListener('click', searchClick);
-$searchBar.addEventListener('input', searchClick);
